740af52d43349bfe62bdc63e308bdbc56db2f07a
naufalgitam <naufalps24@gmail.com>
2023-07-12 20:31:54 +0700

    menambahkan index blade buku

---
+++ resources/views/bukus/index.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen">
+        @if (Route::has('login'))
+            <div class="p-6 text-right">
+                @auth
+                    @include('layouts.navigation')
+                @else
+                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900">Log
+                        in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <h2 class="m-6 text-xl font-semibold text-gray-900 text-center">Daftar Buku</h2>
+
+        <table class="mx-auto">
+            <thead>
+                <tr>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        #
+                    </th>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        Title
+                    </th>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        Author
+                    </th>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        Release Year
+                    </th>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        Salinan Asli
+                    </th>
+                    <th
+                        class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                        Salinan Tersedia
+                    </th>
+                    @auth
+                        <th
+                            class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                            Actions
+                        </th>
+                        @endif
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white">
+                    @foreach ($bukus as $buku)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $loop->index + 1 }}</td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="text-sm font-medium leading-5 text-gray-900">
+                                        {{ $buku->title }}
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-500">{{ $buku->author }}</div>
+                            </td>
+
+                            <td
+                                class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                {{ $buku->year }}
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                @if ($buku->copies_in_circulation < 10)
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-red-800 bg-red-100 rounded-full">
+                                        {{ $buku->copies_in_circulation }}
+                                    </span>
+                                @elseif($buku->copies_in_circulation < 20)
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-orange-800 bg-orange-100 rounded-full">
+                                        {{ $buku->copies_in_circulation }}
+                                    </span>
+                                @else
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                                        {{ $buku->copies_in_circulation }}
+                                    </span>
+                                @endif
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                @if ($buku->availableCopies() < 10)
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-red-800 bg-red-100 rounded-full">
+                                        {{ $buku->availableCopies() }}
+                                    </span>
+                                @elseif($buku->availableCopies() < 20)
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-orange-800 bg-orange-100 rounded-full">
+                                        {{ $buku->availableCopies() }}
+                                    </span>
+                                @else
+                                    <span
+                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                                        {{ $buku->availableCopies() }}
+                                    </span>
+                                @endif
+                            </td>
+                            @auth
+                                <td
+                                    class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                    @if ($buku->canBeBorrowed())
+                                        <a href="{{ route('peminjamen.create', ['buku' => $buku->id]) }}">Pinjam Buku</a>
+                                    @else
+                                        <p class="text-red-600"> No copies available to borrow</p>
+                                    @endif
+                                </td>
+                        @endif
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </body>
+
+        </html>
